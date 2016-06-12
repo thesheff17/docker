b88650230dc1f7ffb5da111419ef3842c1465cb2
Dan Sheffner <Dan@Sheffner.com>
2016-06-12 12:21:21 -0500

    added openvas pull

--- dockerPull.sh
+++ dockerPull.sh
@@ -4,3 +4,4 @@ docker pull sameersbn/mysql:latest
 docker pull jupyter/minimal-notebook:latest
 docker pull thesheff17/dev:latest
 docker pull thesheff17/dev-min:latest
+docker pull mikesplain/openvas:latest
