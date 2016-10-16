878d3083b08f82b2ea4f4d85e52630e12ba1e74a
Dan Sheffner <Dan@Sheffner.com>
2016-10-16 12:14:47 -0500

    added apt-get proxy

--- dockerPull.sh
+++ dockerPull.sh
@@ -4,5 +4,6 @@ docker pull sameersbn/mysql:latest
 docker pull jupyter/minimal-notebook:latest
 docker pull thesheff17/dev:latest
 docker pull thesheff17/dev-min:latest
+docker pull thesheff17/apt-cacher-ng
 docker pull mikesplain/openvas:latest
 docker pull scylladb/scylla
