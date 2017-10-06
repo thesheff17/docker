433cd74a1cf2c27eea6561a23f416fb5461e14fc
Dan Sheffner <Dan@Sheffner.com>
2017-10-05 20:45:56 -0500

    nodejs added versions script

---
+++ dev/versions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+go version
+python --version
+pip --version
+python3 --version
+pip3 --version
+java -version
+php --version
+rustc --version
+cargo --version
+ruby --version
+rails --version
+nodejs --version
+npm --version
